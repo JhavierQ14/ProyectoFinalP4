898ba0024f1df1135ccd724fafb5e855b71b1b51
Javier14 <69540480+carlosjavierjimenezvasquez@users.noreply.github.com>
2022-11-07 20:50:10 -0600

    ActulizacionV

--- app/src/main/java/com/example/proyectofinalprogramacioniv/MainActivity.kt
+++ app/src/main/java/com/example/proyectofinalprogramacioniv/MainActivity.kt
@@ -1,13 +1,24 @@
 package com.example.proyectofinalprogramacioniv
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+
+    }
+
+    fun btnSigResgiter(Vistas: View){
+        val ventanaSiguiente: Intent = Intent(applicationContext,RegistrarseActivity::class.java)
+        startActivity(ventanaSiguiente)
+    }
+
+    fun IniciarSesion(){
 
 
 
