8476ad8ed9f9fa87b6727c9325f9d83b0430cbe1
stanleeeys <69662538+stanleeeys@users.noreply.github.com>
2022-11-09 13:13:41 -0600

    HOme

---
+++ app/src/main/java/com/example/proyectofinalprogramacioniv/DesignHome.kt
@@ -0,0 +1,11 @@
+package com.example.proyectofinalprogramacioniv
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class DesignHome : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_design_home)
+    }
+}
