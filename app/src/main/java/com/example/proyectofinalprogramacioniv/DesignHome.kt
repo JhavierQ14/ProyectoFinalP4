e238608f50352f9606bfde248d1e2956431c09c3
Javier14 <69540480+JhavierQ14@users.noreply.github.com>
2022-11-21 19:09:26 -0600

    VistaQuestion

--- app/src/main/java/com/example/proyectofinalprogramacioniv/DesignHome.kt
+++ app/src/main/java/com/example/proyectofinalprogramacioniv/DesignHome.kt
@@ -10,12 +10,20 @@ import com.example.proyectofinalprogramacioniv.R
 class DesignHome : AppCompatActivity() {
 
     private lateinit var myperfilBtn: LinearLayout
+    private lateinit var questionbtn: LinearLayout
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_design_home)
 
         myperfilBtn = findViewById(R.id.btnMyPerfil)
+        questionbtn = findViewById(R.id.btnQuestionP)
+
+    }
+    fun questionbtn(view: View) {
+
+        val ventanaSiguiente: Intent = Intent(this, QuestionActivity::class.java)
+        startActivity(ventanaSiguiente)
     }
 
     fun MyPerfil(view: View) {
---
+++ app/src/main/java/com/example/proyectofinalprogramacioniv/QuestionActivity.kt
@@ -0,0 +1,22 @@
+package com.example.proyectofinalprogramacioniv
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.LinearLayout
+
+class QuestionActivity : AppCompatActivity() {
+
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_question)
+
+
+    }
+
+
+}
