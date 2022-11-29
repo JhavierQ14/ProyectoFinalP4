510dcea08950a4468cabd5b17f81ec0675676fdf
Javier14 <69540480+JhavierQ14@users.noreply.github.com>
2022-11-28 20:36:33 -0600

    Arreglos

--- app/src/main/java/com/example/proyectofinalprogramacioniv/ConfigActivity.kt
+++ app/src/main/java/com/example/proyectofinalprogramacioniv/ConfigActivity.kt
@@ -12,13 +12,11 @@ import com.example.proyectofinalprogramacioniv.Domain.ClsAuthentication
 
 class ConfigActivity : AppCompatActivity() {
 
-    private lateinit var regresarH:Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_config)
 
-        regresarH = findViewById(R.id.Regresarh)
 
         val swiitch = findViewById<Switch>(R.id.swtich)
         val ivModo = findViewById<ImageView>(R.id.ivModo)
