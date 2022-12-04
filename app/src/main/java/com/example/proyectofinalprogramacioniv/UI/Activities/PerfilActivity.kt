32c129b4bbbb7913bb386cbab9d24506528f993f
ha0372 <ha0372032020@unab.edu.sv>
2022-12-03 19:54:09 -0600

    binding

--- app/src/main/java/com/example/proyectofinalprogramacioniv/UI/Activities/PerfilActivity.kt
+++ app/src/main/java/com/example/proyectofinalprogramacioniv/UI/Activities/PerfilActivity.kt
@@ -2,36 +2,35 @@ package com.example.proyectofinalprogramacioniv.UI.Activities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.provider.ContactsContract.CommonDataKinds.Email
+import android.view.View
 import android.widget.EditText
 import android.widget.TextView
-import com.example.proyectofinalprogramacioniv.R
+import com.example.proyectofinalprogramacioniv.databinding.ActivityPerfilBinding
 import com.google.firebase.auth.ktx.auth
 import com.google.firebase.firestore.FirebaseFirestore
 import com.google.firebase.ktx.Firebase
 
 class PerfilActivity : AppCompatActivity() {
 
+    //binding
+    private lateinit var _binding: ActivityPerfilBinding
 
-    private lateinit var nameUserTv: TextView
-    private lateinit var  EmailUserTV: TextView
-    private lateinit var emailUserEdt: EditText
-    private  lateinit var  NameUserEdt: EditText
-    private  lateinit var  LastNameUserEdt: EditText
-
+    //
     private var db = FirebaseFirestore.getInstance()
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_perfil)
 
-        emailUserEdt = findViewById(R.id.edtEmailUser)
-        nameUserTv = findViewById(R.id.tvNameUser)
-        EmailUserTV = findViewById(R.id.tvemailUser)
-        NameUserEdt = findViewById(R.id.edtNombreUser)
-        LastNameUserEdt = findViewById(R.id.edtLastNameUser)
+        //Implentacion de binding para conectar con vistas o elementos
+        _binding = ActivityPerfilBinding.inflate(layoutInflater)
+        setContentView(_binding.root)
 
+        //
+        GetInfoUser()
+    }
 
+    private fun GetInfoUser(){
 
         val user = Firebase.auth.currentUser
         user?.let {
@@ -48,27 +47,69 @@ class PerfilActivity : AppCompatActivity() {
             // FirebaseUser.getToken() instead.
             val uid = user.uid
 
-            emailUserEdt.setText(email)
+            //emailUserEdt.setText(email)
+            _binding.tvemailUser.setText(email)
 
-            db.collection("Users").document(email.toString()).get().addOnSuccessListener{
-                nameUserTv.setText(it.get("nameUser"+ " " + "lastNameUser") as String?)
-                NameUserEdt.setText(it.get("nameUser")as String?)
-                LastNameUserEdt.setText(it.get("lastNameUser") as String?)
+            db.collection("Users").document(email.toString()).get().addOnSuccessListener {
 
+                var n = it.get("nameUser"+ " " + "lastNameUser") as String?
+                var nn = ""
+                _binding.tvNameUser.setText(n)
+                //nameUserTv.setText(it.get("nameUser"+ " " + "lastNameUser") as String?)
+                _binding.edtNombreUser.setText(it.get("nameUser")as String?)
+                //NameUserEdt.setText(it.get("nameUser")as String?)
+                _binding.edtLastNameUser.setText(it.get("lastNameUser") as String?)
+                //LastNameUserEdt.setText(it.get("lastNameUser") as String?)
 
             }
-
-
         }
     }
 
-    fun GetInfo(){
+    fun Loquesea(v: View){
 
-        var email = emailUserEdt.text.toString()
-        var name = nameUserTv.text.toString()
+       /* val collectionQuestion = db.collection("Questions")
 
+        val data1 = hashMapOf(
+            "cod_state_test" to 1,
+            "test_name" to "Teorico",
+            "img_question" to "urlimg",
+            "description_question" to "En las vías de doble sentido de circulación y tres carriles separados por marcas longitudinales discontinuas, debemos circular por el carril:",
+            "answer_question" to listOf("No esta reglamentado",
+                "No esta reglamentado",
+                "Por el centro de la vía",
+                "Por el carril izquierdo",
+                "Por el carril derecho",
+                "2")
+        )
+        collectionQuestion.document().set(data1)
 
+        val data2 = hashMapOf(
+            "cod_state_test" to 1,
+            "test_name" to "Teorico",
+            "img_question" to "urlimg",
+            "description_question" to "Cuál es el color utilizado en las señales informativas",
+            "answer_question" to listOf("west_coast",
+                "Rojas con blanco",
+                "violeta Azul",
+                "blanco Amarillas con negro Café",
+                "azul y verde",
+                "2")
+        )
+        collectionQuestion.document().set(data2)
 
-
+        val data3 = hashMapOf(
+            "cod_state_test" to 1,
+            "test_name" to "Teorico",
+            "img_question" to "urlimg",
+            "description_question" to "Uno de los requisitos que deben tener los talleres que realizan la revisión mecánica es:",
+            "answer_question" to listOf("Deben ser talleres del Gobierno",
+                "Deben dedicarse solo a estas revisiones",
+                "Poseer equipo técnico adecuado para efectuar dichas revisiones",
+                "Deben estar fuera de la capital",
+                "Deben ser talleres del Gobierno",
+                "1")
+        )
+        collectionQuestion.document().set(data3)*/
     }
+
 }
